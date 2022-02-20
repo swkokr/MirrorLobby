5411dbb7486f6a0364a5ead0c5cd8748302a642d
swkokr <swkokr@gmail.com>
2022-02-20 17:41:09 +0900

    first commit

---
+++ Assets/Scripts/UILobby.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILobby : MonoBehaviour
+{
+   [SerializeField] InputField jointMatchInput; 
+
+   public void Host(){
+
+   }
+
+   public void Join(){
+
+   }
+}
